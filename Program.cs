446f5718c4ee43fa94cad073c0f9c2a11513b718
osci-the-orca <88oscste@skola.boras.se>
2021-09-30 11:38:40 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace h5chocolate_teambla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("H5Choclate!");
+        }
+    }
+}

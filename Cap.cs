72c46559c86130e4b82013480aa77201c3692b03
Daniel T <90195897+DTSD21@users.noreply.github.com>
2021-10-04 14:22:34 +0200

    new choco method

--- Cap.cs
+++ Cap.cs
@@ -2,6 +2,6 @@ namespace h5chocolate_teambla
 {
     class Cap : Product
     {
-        
+        //sätt standardvariabler, ska metod för att sätta övriga kepspecifika variabler med while loop eller switch, med en submeny
     }
 }
--- Chocolate.cs
+++ Chocolate.cs
@@ -4,6 +4,25 @@ namespace h5chocolate_teambla
 {
     class Chocolate : Product
     {
-       
+        //attribut - fyllning, kakaomängd, 
+        private byte Cocoa; //10-90%
+        private string Filling; //t.ex. guldflarn eller ingen fyllning, ska man kunna ha flera 
+        private bool TestCheck;
+        private byte AmountOfBars;
+        private string UserInput;
+
+        public Chocolate CreateChocolate(byte Cocoa,string Filling, byte AmountOfBars)
+        {
+            while (true)
+            {
+                Console.WriteLine("How much cocoa content would you like your chocolate bar to have? Enter an amount (10-90%).");
+                TestCheck = byte.TryParse(UserInput=Console.ReadLine(), out Cocoa);
+                Console.WriteLine(Cocoa);
+                break;
+            }
+            Chocolate newChocolate = new();
+            return newChocolate;
+        }
+       //sätt standardvariabler, ska metod SOM HETER CREATECHOCOLATE(), koppla ihop med Menu för att sätta övriga chokladspecifika variabler med while loop eller switch, med en submeny, skicka tillbaka med return till menyn
     }
 }

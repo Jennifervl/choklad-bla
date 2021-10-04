a2b2c0d0bd863fd7bd48b86f1522de371912ecda
osci-the-orca <88oscste@skola.boras.se>
2021-10-04 12:37:32 +0200

    working on menu and order classes

--- Menu.cs
+++ Menu.cs
@@ -12,5 +12,58 @@ namespace h5chocolate_teambla
             User createdUser = new User(userInput);
             return createdUser;
         }
+
+        public static Order CreateNewOrder()
+        {
+            Order newOrder = new Order(0, 0, "");
+            Console.WriteLine("[1] Chocolate");
+            Console.WriteLine("[2] Cap");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Chocolate.createChocolate();
+
+
+            }
+            else if (choice == "2")
+            {
+
+            }
+
+            return newOrder;
+        }
+
+        public void ShowMenu()
+        {
+            Console.WriteLine("[1] Place order");
+            Console.WriteLine("[2] Order history");
+            Console.WriteLine("[3] Exit");
+
+            string menuChoice = Console.ReadLine();
+
+            switch (menuChoice)
+            {
+                case "1":
+                    {
+
+                        Order.CreateOrder();
+                        break;
+                    }
+
+                case "2":
+                    {
+                        User.printHistory();
+                        break;
+                    }
+
+                case "3":
+                    Environment.Exit(0);
+                    break;
+            }
+
+
+        }
     }
 }

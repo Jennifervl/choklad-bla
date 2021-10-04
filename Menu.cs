bd0fdfa245fc0c564557b043ccf9a437112de67f
Daniel Berg <Berg.ioDaniel@gmail.com>
2021-10-04 11:41:58 +0200

    users started

--- Menu.cs
+++ Menu.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace h5chocolate_teambla
 {
-    class Menu
+    static class Menu
     {
+        public static User CreateNewUser()
+        {
+            Console.WriteLine("Enter ID: ");
+            string userInput = Console.ReadLine();
 
+            User createdUser = new User(userInput);
+            return createdUser;
+        }
     }
 }
--- UserList.cs
+++ UserList.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace h5chocolate_teambla
 {
     public class UserList
     {
-        
+        private List<User> userList = new List<User>();
+
+        public void AddUser(User user)
+        {
+            userList.Add(user);
+        }
+        public IList<User> GetList()
+        {
+            IList<User> privateList = userList.AsReadOnly();
+            return privateList;
+        }
     }
 }
